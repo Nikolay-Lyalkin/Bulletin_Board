3691993c21ca7b970b7f502fa4eb720cceeec123
Nikolay_Lyalkin <serega94nn@yandex.ru>
2025-03-23 00:15:43 +0300

    ci

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 PATH_RESET_PASSWORD = "http://127.0.0.1:8000/user/reset_password_confirm"
 
